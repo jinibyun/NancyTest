9672023bf4adf1d5d0deb66be08b1a2eba13ae87
Jini Byun <jinibyun@gmail.com>
2019-01-29 22:13:55 -0500

    commit

---
+++ BaconIpsumMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NancyTest
+{
+    public class BaconIpsumMessage
+    {
+        public string Body { get; set; }
+    }
+
+    public interface IBaconIpsumService
+    {
+        Task<BaconIpsumMessage> GenerateAsync();
+    }
+
+    public class BaconIpsumService : IBaconIpsumService
+    {
+        public async Task<BaconIpsumMessage> GenerateAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var message = new BaconIpsumMessage();
+                // Post the message
+                message.Body = await client.GetStringAsync(
+                    $"https://baconipsum.com/api/?type=meat-and-filler");
+
+                return message;
+            }
+        }
+    }
+}
---
+++ BaconIpsumModule.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+
+
+namespace NancyTest
+{
+    public class BaconIpsumModule: NancyModule
+    {
+        public BaconIpsumModule(IBaconIpsumService baconIpsumService)
+        {
+            Get("/", args => "Super Duper Happy Path running on .NET Core");
+            Get("/baconipsum", async (args) => await baconIpsumService.GenerateAsync());
+        }
+    }
+}
